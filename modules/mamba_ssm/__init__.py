51d41ce8e0e477f5c095a56fcbddf611fc38e2c5
CHARAN422176 <422176@student.nitandhra.ac.in>
2025-08-01 18:10:09 +0530

    commit

--- modules/mamba_ssm/__init__.py
+++ modules/mamba_ssm/__init__.py
@@ -1,8 +1,8 @@
 __version__ = "1.2.0.post1"
 
-from mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
+from .mamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 # from .ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
-from mamba_ssm.modules.mamba_simple import Mamba
+from .mamba_ssm.modules.mamba_simple import Mamba
 # from .modules.mamba_simple import Mamba
-from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+from .mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
 # from .models.mixer_seq_simple import MambaLMHeadModel
